bbfe0649909e784f242892e12e88b8222f7a2da4
MailsonD <mailssondennis@gmail.com>
2019-03-23 20:08:50 -0300

    Resolvendo problema de conexão com o mongo

---
+++ databases/connectionMongo.php
@@ -0,0 +1,16 @@
+<?php
+
+//É necessário importar via linha de comando
+//$ composer require mongodb/mongodb
+
+//este comando só funcionará após rodar o composer no temrinal
+require_once __DIR__ . "/vendor/autoload.php";
+
+function getConnectionMongo(){
+
+	//teste é o nome do banco
+	$db = (new MongoDB\Client)->teste;
+
+}
+
+?>
