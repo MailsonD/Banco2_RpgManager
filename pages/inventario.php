a6938f63d5ae46a08df403f0037decf5f8dd22ef
camiula <camilacarwalho3@gmail.com>
2019-03-23 15:25:36 -0300

    Itens no inventário

--- pages/inventario.php
+++ pages/inventario.php
@@ -1,13 +1,56 @@
 
 <!-- Modal Structure -->
 <div id="modal3" class="modal">
-    <div class="modal-content">
-        <center>
-            <div class="row">
-              <h3 class="card-title">Inventário</h3>
-            
-            </div>
-        </center>
-    </div>
+   
+
+<h3 class="card-title">Inventário</h3>
+<div class="row">
+    <div class="card col s5 m3">
+    <div class="card-content">
+      <img src="../assets/img/potion.png" class="item" alt="">
+      <div class="card-action">
+      <span class="title">Item 1</span>
+      <p>Descrição <br>
+        Desrição 2
+      </p></div></div></div>
+
+      <div class="card col s5 m3">
+    <div class="card-content">
+      <img src="../assets/img/potion.png" class="item" alt="">
+      <div class="card-action">
+      <span class="title">Item 1</span>
+      <p>Descrição <br>
+        Desrição 2
+      </p></div></div></div>
+
+       <div class="card col s5 m3">
+    <div class="card-content">
+      <img src="../assets/img/potion.png" class="item" alt="">
+      <div class="card-action">
+      <span class="title">Item 1</span>
+      <p>Descrição <br>
+        Desrição 2
+      </p></div></div></div>
+
+       <div class="card col s5 m3">
+    <div class="card-content">
+      <img src="../assets/img/potion.png" class="item" alt="">
+      <div class="card-action">
+      <span class="title">Item 1</span>
+      <p>Descrição <br>
+        Desrição 2
+      </p></div></div></div>
+
+      <div class="card col s5 m3">
+    <div class="card-content">
+      <img src="../assets/img/potion.png" class="item" alt="">
+      <div class="card-action">
+      <span class="title">Item 1</span>
+      <p>Descrição <br>
+        Desrição 2
+      </p></div></div></div>
+   
+</div>
+
 
 </div>
