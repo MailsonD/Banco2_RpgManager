fd1ee3fc7c291b9841cc9234e61f9363c803d1e4
camiula <camilacarwalho3@gmail.com>
2019-03-24 15:12:52 -0300

    Arrumando grid com borda noca

--- pages/home.php
+++ pages/home.php
@@ -10,9 +10,10 @@
     <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/style.css" />
 </head>
 <body>
-<img src="../assets/img/gradient.jpg" class="degrade">
+
+<img src="../assets/img/gradient.jpg" class="degrade"><img src="../assets/img/header.png" class="stroke">
 <div class="row">
-    <div class="col s12 m2">       
+    <div class="col s12 m2 painel">       
       <div class="card  transparent"><a href="/pages/home.php"><img src="../assets/img/rpgLOGO.png" class="logo center-align"></a>
          <div class="card-action center-align">
           <a class="waves-effect waves-light btn-large red darken-4">Aventura</a><br><br>
@@ -28,9 +29,9 @@
       </div>
     </div>
 
-    <div id="test1" class="col s12 m6" > <?php include("personagens.php"); ?></div>
-    <div id="test4" class="col s12 m6"><?php include("criarpersonagem.php"); ?></div>
-    <div class="col s7 m4 ">
+    <div id="test1" class="col s12 m6 painel" > <?php include("personagens.php"); ?></div>
+    <div id="test4" class="col s12 m6 painel"><?php include("criarpersonagem.php"); ?></div>
+    <div class="col s7 m4 painel">
       <div class="card center-align char transparent">
        
             
@@ -50,7 +51,6 @@
         <ul class="tabs grey darken-4"> 
       
         <li class="tab"><a href="#test1">Meus heróis</a></li>
-  <li class="tab"><a class="active" href=""></a></li>
         <li class="tab"><a href="#test4">Criar novo</a></li>
       </ul>
          
