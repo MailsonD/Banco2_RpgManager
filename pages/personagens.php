f5038cd8d3935e5ab47b986a4a9a6122bacee254
camiula <camilacarwalho3@gmail.com>
2019-03-24 12:58:03 -0300

    Listagem de personagens

--- pages/personagens.php
+++ pages/personagens.php
@@ -1,11 +1,12 @@
 
     
-      <div class="card grey darken-4 white-text center-align">
+      <div class="card horizontal grey darken-4 white-text center-align">
         <div class="card-content">
-          <span class="card-title">Personagens</span>
-          <p>Aqui vou listar os personagens </p>
+        <img src='/assets/img/char.png' class="avatar">
+          <span class="card-title">Leoric</span>
+         
         </div>
-        <div class="card-action">
+        <div class="card-action"> <p>Aqui vou listar os personagens </p>
         <a class="waves-effect waves-light btn-small red darken-4" href="#">Escolher</a>
           <a href="#">Editar</a>
           <a href="#">Deletar</a>
