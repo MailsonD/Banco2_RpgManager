564986c63a6168a61ada3eac622e8e79db59c164
MailsonD <mailssondennis@gmail.com>
2019-03-24 15:59:46 -0300

    Adicionando mapeamento ao formulário de cadastro e adição de select

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
     <link rel="stylesheet" type="text/css" media="screen" href="assets/css/materialize.min.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
     <?php
-        session_start();
-     ?>
+    session_start();
+    ?>
 </head>
 <body>
-<div class="container">
+    <div class="container">
         <br><br>
         <h1 class="header center white-text">RPG</h1>
         <div class="row center">
@@ -54,36 +54,45 @@
                 <div class="modal-content">
                     <center>
                         <div class="row">
-                            <form class="col10 s10">
+                            <form class="col10 s10" method="POST" action="services/cadastro.php">
                                 <div class="row">
-                                  
-                                   <div class="input-field col10 s10">
-                                        <input id="name" type="text" class="validate" name="nome"maxlength="40" required>
-                                       <label for="name">Nome<font color="red"> * </font></label></div>
-                                    <div class="input-field col10 s10">
-                                        <input id="email" type="email" class="validate" name="email"required>
-                                        <label for="email">E-mail<font color="red"> * </font></label></div>
-                                    <div class="input-field col10 s10">
-                                        <input id="password" type="password" class="validate" name="senha" minlength="6" required>
-                                        <label for="password">Senha<font color="red"> * </font></label></div>
-                                    <div class="input-field col10 s10">
-                                        <input id="date" type="date" class="validate" name="nascimento" required>
-                                        <label for="date">Data de Nascimento<font color="red"> * </font></label>
-                                    </div>
 
-
-                                    <button class="btn waves-effect waves-light" type="submit" name="action">Cadastrar
-                                    </button>
+                                 <div class="input-field col10 s10">
+                                    <input id="name" type="text" class="validate" name="nome"maxlength="40" required>
+                                    <label for="name">Nome<font color="red"> * </font></label>
                                 </div>
-                            </form>
-                        </div>
-                    </center>
-                </div>
+                                <div class="input-field col10 s10">
+                                    <input id="email" type="email" class="validate" name="email"required>
+                                    <label for="email">E-mail<font color="red"> * </font></label>
+                                </div>
+                                <div class="input-field col10 s10">
+                                    <input id="password" type="password" class="validate" name="senha" minlength="6" required>
+                                    <label for="password">Senha<font color="red"> * </font></label>
+                                </div>
+                                <div class="input-field col10 s10">
+                                    <select required name="experiencia">
+                                        <option value="Novato" selected="true">Novato</option>
+                                        <option value="Experiênte">Experiênte</option>
+                                        <option value="Especialista">Especialista</option>
+                                    </select>
+                                    <label>Nível de experiência
+                                        <font color="red"> * </font>
+                                    </label>
+                                </div>
 
+
+                                <button class="btn waves-effect waves-light" type="submit" name="action">Cadastrar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </center>
             </div>
+
         </div>
+    </div>
 </div>
-    
+
 <!--  Scripts-->
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -95,6 +104,7 @@
 <script>
     $(document).ready(function () {
         $('.modal').modal();
+        $('select').formSelect();
         <?php include("services/msgErro.php")?>
     });
 </script>
