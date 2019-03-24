0a1aab583588a5c2ec34dec98da5b6f522d978fe
camiula <camilacarwalho3@gmail.com>
2019-03-24 15:30:06 -0300

    Início aventura

---
+++ pages/aventura.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>RPG</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/materialize.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/materialize.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/game.css" />
+</head>
+<body>
+<img src="../assets/img/gradient.jpg" class="degrade">
+
+</body>
+</html>
