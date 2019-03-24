6e774c6900f66a0d389cd57d4e7cf6a6108e687c
MailsonD <mailssondennis@gmail.com>
2019-03-24 16:22:07 -0300

    Funcinalidade para manter o usuário logado na sessão

--- services/autentication.php
+++ services/autentication.php
@@ -16,7 +16,8 @@
 
 
 	if(!$result==null){
-		header('location:pages/home.php'); 
+		$_SESSION['logado'] = $email;
+		header('location:../pages/home.php'); 
 	}else{
 		$_SESSION['msg'] = "Erro de autenticação";
 		header('location:../index.php'); 
