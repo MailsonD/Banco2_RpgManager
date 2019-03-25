c765f93e9995d9953de4e07f373f4164a33b1d92
MailsonD <mailssondennis@gmail.com>
2019-03-25 11:00:48 -0300

    Adicionando documentos dentro de documentos através da aplicação

---
+++ services/criarPersonagem.php
@@ -0,0 +1,31 @@
+<?php
+
+require("../databases/mongoDao.php");
+
+session_start();
+
+$emailLogado = $_SESSION['logado'];
+
+$nome = $_POST['nome'];
+$raca = $_POST['raca'];
+$naturalidade  = $_POST['naturalidade'];
+$adoracao = $_POST['adoracao'];
+
+$document = [
+				'personagens'=>[
+									[
+										'nome'=>$nome,
+										'raca'=>$raca,
+										'naturalidade'=>$naturalidade,
+										'adoracao'=>$adoracao,
+									],
+								],	
+			];				
+
+$condicao = [
+				'email'=>$emailLogado,
+			];
+
+$result = atualizarMDB('usuario',$condicao,$document);				
+
+?>
