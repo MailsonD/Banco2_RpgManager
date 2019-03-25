cc7a7e8a0e75fe6c94c652b8d3527dfec649c98d
MailsonD <mailssondennis@gmail.com>
2019-03-25 09:16:50 -0300

    Adicionando outros campos ao formulário de criação de persoangens

--- pages/criarpersonagem.php
+++ pages/criarpersonagem.php
@@ -1,43 +1,50 @@
    <div class="card grey darken-4 white-text center-align">
-        <div class="card-content">
+    <div class="card-content">
         <h6>Dados do personagem</h6>
         <form>
-                              
-                                  
-                                   <div class="input-field col10 s10">
-                                        <input id="name" type="text" name="nome"maxlength="40" required>
-                                       <label for="name">Nome</label></div>
-                                    <div class="input-field col10 s10">
-                                        <input id="raca" type="text" class="validate">
-                                        <label for="raca">Raça</label></div>
-                                      
-                                        <div class="input-field col10 s10">
-                                        <input id="naturalidade" type="text" class="validate" >
-                                        <label for="naturalidade">Naturalidade</label></div>
-                                        <div class="input-field col10 s10">
-                                        <input id="vocacao" type="text" class="validate" >
-                                        <label for="vocacao">Vocação</label></div>
-                                   
-                                        <h6>Atributos</h6>
-         <form class="col s10"> 
-         <div class="row">
-         <div class="input-field col s5">
-                                        <input id="forca" type="number" class="validate" >
-                                        <label for="forca">Força</label></div>
-          <div class="input-field col s5">
-                                        <input id="destreza" type="number" class="validate" >
-                                        <label for="destreza">Destreza</label></div>
-                                        </div>
-         </form>
-    
+
+
+           <div class="input-field col10 s10">
+            <input id="name" type="text" name="nome" class="validate white-text" maxlength="40" required>
+            <label for="name">Nome</label>
         </div>
-                                 
-                                 
-                                 
-                            </form>
-                            <button class="waves-effect waves-light btn-large red darken-4" type="submit" name="action">Criar
-  </button>
-         
-      </div>
-   
- 
+        <div class="input-field col10 s10">
+            <input id="raca" type="text" name="raca" class="validate white-text" required>
+            <label for="raca">Raça</label>
+        </div>
+
+        <div class="input-field col10 s10">
+            <input id="naturalidade" type="text" name="naturalidade" class="validate white-text"  required>
+            <label for="naturalidade">Naturalidade</label>
+        </div>
+        <div class="input-field col10 s10">
+            <select class="validate white-text" required name="Divindade">
+                <option class="white-text" value="Odin" selected="true">Odin</option>
+                <option class="white-text" value="Freya">Freya</option>
+                <option class="white-text" value="Ymir">Ymir</option>
+                <option class="white-text" value="Camila">Camila</option>
+            </select>
+            <label>Adoração</label>
+        </div>
+
+        <h6>Atributos</h6> 
+        <div class="row">
+         <div class="input-field col s5">
+            <input id="forca" type="number" class="validate white-text" min="0">
+            <label for="forca">Força</label>
+        </div>
+        <div class="input-field col s5">
+            <input id="destreza" type="number" class="validate white-text" min="0">
+            <label for="destreza">Destreza</label></div>
+        </div>
+
+    </div>
+
+
+
+</form>
+<button class="waves-effect waves-light btn-large red darken-4" type="submit" name="action">Criar
+</button>
+
+</div>
+
