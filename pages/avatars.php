8685626bd66b7c016c33be7bfa4dc5ce1badea34
camiula <camilacarwalho3@gmail.com>
2019-03-25 22:19:29 -0300

    Modal para escolha de avatar

--- pages/avatars.php
+++ pages/avatars.php
@@ -2,37 +2,62 @@
 <!-- Modal Structure -->
 <div id="avatar" class="modal">
    
-
-<h3 class="card-title">Inventário</h3>
 <div class="row">
 <form>
     <div class="card col s5 m4">
     <div class="card-content">
       <img src="../assets/img/char.png" class="item" alt="">
+      <label>
+        <input name="1" type="radio" />
+        <span>1</span>
+      </label>
      </div></div>
 
       <div class="card col s5 m4">
     <div class="card-content">
       <img src="../assets/img/char2.png" class="item" alt="">
+      <label>
+        <input name="2" type="radio" />
+        <span>2</span>
+      </label>
      </div></div>
      <div class="card col s5 m4">
     <div class="card-content">
       <img src="../assets/img/char3.png" class="item" alt="">
+      <label>
+        <input name="3" type="radio"/>
+        <span>3</span>
+      </label>
      </div></div>
 
       <div class="card col s5 m4">
     <div class="card-content">
       <img src="../assets/img/char4.png" class="item" alt="">
+      <label>
+        <input name="4" type="radio"/>
+        <span>4</span>
+      </label>
      </div></div>
 
      <div class="card col s5 m4">
     <div class="card-content">
       <img src="../assets/img/char5.png" class="item" value="5s" alt="">
+      <label>
+        <input name="5" type="radio"/>
+        <span>5</span>
+      </label>
      </div></div>
      <div class="card col s5 m4">
     <div class="card-content">
       <img src="../assets/img/char6.png" class="item" value="6" alt="">
+      <label>
+        <input name="6" type="radio"/>
+        <span>6</span>
+      </label>
      </div></div>
+
+<button class="waves-effect waves-light btn-small red darken-4" type="submit" name="action">Escolher
+</button>
 
      </form>
    
