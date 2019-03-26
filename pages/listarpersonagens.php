043c01f61fef1aa4aac3751d43c53809455e89aa
camiula <camilacarwalho3@gmail.com>
2019-03-25 22:57:11 -0300

    Listagem de personagens. ainda não funcional

--- pages/listarpersonagens.php
+++ pages/listarpersonagens.php
@@ -1,6 +1,6 @@
 
     
-    <div class="card horizontal grey darken-4 white-text center-align">
+    <div class="card horizontal grey darken-4 white-text center-align" action="../services/listarpersonagens.php">
       
     <?php include("personagens.php"); ?>
       </div>
--- pages/personagens.php
+++ pages/personagens.php
@@ -1,9 +1,11 @@
+
+    <?php include("../services/listarpersonagens.php"); ?>
 <div class="card-content">
         <img src='/assets/img/char.png' class="avatar">
-          <span class="card-title">Leoric</span>
+          <span class="card-title"><?php echo $nome ?></span>
          
         </div>
-        <div class="card-action"> <p>Raça</p><p>Naturalidade<p><p>Adoração</p>
+        <div class="card-action"> <p><?php echo $raca ?></p><p><?php echo $naturalidade ?><p><p><?php echo $adoracao ?></p>
        <hr><br><br>
         <a class="waves-effect waves-light btn-small red darken-4" href="#">Escolher</a>
           <a href="#">Editar</a>
