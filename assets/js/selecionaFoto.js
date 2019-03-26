38845e5ac6f20237f39571ee4b07f54f4634d4cd
MailsonD <mailssondennis@gmail.com>
2019-03-26 09:19:38 -0300

    Adicionando Script pra seleção de fotos

---
+++ assets/js/selecionaFoto.js
@@ -0,0 +1,9 @@
+
+function escolherFoto(){
+	var radios = document.getElementsByName("foto");
+	for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            console.log("Escolheu: " + radios[i].value);
+        }
+    }
+}
