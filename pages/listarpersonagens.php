7471d5be1b7e53fe650a92292de562d61f96c5bd
MailsonD <mailssondennis@gmail.com>
2019-03-27 20:10:21 -0300

    Organizando atributos de listagem de personagens

--- pages/listarpersonagens.php
+++ pages/listarpersonagens.php
@@ -1,8 +1,8 @@
 
-    
-    <div class="card horizontal grey darken-4 white-text center-align">
-      
-    <?php include("../services/listarpersonagens.php"); ?>
-     </div>
-   
- 
+
+<div class="card horizontal grey darken-4 white-text center-align">
+	<div class="row"> 
+		<?php include("../services/listarpersonagens.php"); ?>
+	</div>
+</div>
+
--- services/listarpersonagens.php
+++ services/listarpersonagens.php
@@ -18,15 +18,20 @@ $personagens = $result['personagens'];
 foreach($personagens as $document){            
 
 	?>
-	<div class="card-content">
+	<div class="col s4">
+		<div class="card-content">
 		<img src=<?php echo '/assets/img/'.$document['avatar'].''; ?> class="avatar">
 		<span class="card-title"><?php echo $document['nome']; ?></span>
 
 	</div>
-	<div class="card-action"> <p><?php echo $document['raca']; ?></p><p><?php echo $document['classe']; ?><p><p><?php echo $document['adoracao']; ?></p>
+	<div class="card-action">
+		<p><?php echo "<b>Raça:</b> ".$document['raca']; ?></p>
+		<p><?php echo "<b>Classe:</b> ".$document['classe']; ?></p>
+		<p><?php echo "<b>Adoração:</b> ".$document['adoracao']; ?></p>
 		<hr><br><br>
 		<a class="waves-effect waves-light btn-small red darken-4" href="#">Escolher</a>
 
+	</div>
 	</div>
 
 
