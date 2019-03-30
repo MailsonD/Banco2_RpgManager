8eb088ae550262c755f443b064b0c8bf2718c93d
MailsonD <mailssondennis@gmail.com>
2019-03-30 14:55:49 -0300

    Corrigindo bug de invalidate na sessão, mesmo estando logado

--- controllers/sessionController.php
+++ controllers/sessionController.php
@@ -9,6 +9,7 @@ session_start();
 if(!isset ($_SESSION['logado'])){
 	unset($_SESSION['logado']);
 	header('location:../index.php');
+	exit();
 }
 
 $logado = $_SESSION['logado'];
--- controllers/sessionControllerLogged.php
+++ controllers/sessionControllerLogged.php
@@ -1,9 +1,13 @@
 <?php
 
+require_once __DIR__ . "/../vendor/mongodb/mongodb/src/Model/BSONDocument.php";
+
+
 session_start();
 
 if(isset ($_SESSION['logado'])){
 	header('location:../pages/home.php');
+	exit();
 }
 
 ?>
