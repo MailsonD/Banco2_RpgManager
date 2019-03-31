1de5d75544baefb89b2e190111624ba57f9c0758
camiula <camilacarwalho3@gmail.com>
2019-03-31 19:04:42 -0300

    Aumentando tamanho na tela de chat

--- pages/chat.php
+++ pages/chat.php
@@ -3,11 +3,11 @@
     <div class="card horizontal grey darken-4 white-text center-align">
         <div class="card-content">
           <span class="card-title">Chat</span>
-          <div class="card grey darken-3 white-text center-align">
+          <div class="card grey darken-3 white-text center-align chatContent">
               <br></p><br>
           </div> 
 
-                            <div class="input-field">
+                            <div class="input-field chatMessages">
                                 <input id="comentario" type="text" name="comentario">
                                 <label for="comentario" id="mensagem"></label> <button class="btn-floating waves-effect waves-light red" type="submit" name="action"><i
                                     class="material-icons right">chat</i>
