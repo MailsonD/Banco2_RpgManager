d7ab12e7c45ea0da56c1be8eb3a577a2c6f03ba3
camiula <camilacarwalho3@gmail.com>
2019-04-01 09:21:52 -0300

    Mudando modal de dado no modo aventura

--- pages/dado.php
+++ pages/dado.php
@@ -1,9 +1,9 @@
 <div id="dado" class="modal col s2 m3">
+    <br>
 
-   <a class="waves-effect btn-small btn red darken-4 " id="sortear">Sortear</a> 
+   
    <p>Seu número é:</p><br>
    <hr class="style-two"> 
     <h5 id="mensagem"></h5>
+    <a class="waves-effect btn-small btn red darken-4 " id="sortear">Sortear</a> 
 </div>
-
-<script src="../assets/js/dado.js"></script>
