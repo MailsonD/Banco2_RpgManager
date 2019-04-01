45525b276838be30cc8c86612841581dc5c859d2
camiula <camilacarwalho3@gmail.com>
2019-04-01 09:34:20 -0300

    Editando loja

--- pages/loja.php
+++ pages/loja.php
@@ -11,7 +11,6 @@
       <div class="card-action">
       <span class="title"><b>Poção de vida</b></span>
       <p>Aumenta a vida em +20<br>
-        <a href="">Comprar</a>
       </p></div></div></div>
 
      <div class="card col s4 m3">
@@ -20,7 +19,6 @@
       <div class="card-action">
       <span class="title"><b>Poção de mana</b></span>
       <p>Aumenta a magia em +20<br>
-        <a href="">Comprar</a>
       </p></div></div></div>
 
       <div class="card col s4 m3">
@@ -29,7 +27,6 @@
       <div class="card-action">
       <span class="title"><b>Botas</b></span>
       <p>Aumenta a velocidade em +20<br>
-        <a href="">Comprar</a>
       </p></div></div></div>
 
        <div class="card col s4 m3">
@@ -38,7 +35,6 @@
       <div class="card-action">
       <span class="title"><b>Armadura</b></span>
       <p>Aumenta a resistência em +20<br>
-        <a href="">Comprar</a>
       </p></div></div></div>
 
       <div class="card col s4 m3">
@@ -47,7 +43,6 @@
       <div class="card-action">
       <span class="title"><b>Escudo paladino</b></span>
       <p>Aumenta a defesa em +20<br>
-        <a href="">Comprar</a>
       </p></div></div></div>
    
 </div>
