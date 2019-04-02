f1cab095bade2f6842b2bade97465d56276a3896
MailsonD <mailssondennis@gmail.com>
2019-04-02 13:52:06 -0300

    Envio de mensagens com o resultado de um dado

--- assets/js/dado.js
+++ assets/js/dado.js
@@ -1,22 +1,34 @@
 
 function randomIntFromInterval(min,max)
 {
-    
-    return Math.floor(Math.random()*(max-min+1)+min);
+
+	return Math.floor(Math.random()*(max-min+1)+min);
 }
 function dado(){
 
-var tipoDado=document.getElementById("valoresDados").value;
-document.getElementById("seunumero").innerHTML = "Seu número é:";
-if (tipoDado== "D4") {   
-document.getElementById("mensagem").innerHTML = randomIntFromInterval(1,4);
-} else if (tipoDado=="D12") {
-document.getElementById("mensagem").innerHTML =randomIntFromInterval(1,12);
-  }else if (tipoDado=="D10") {
-    document.getElementById("mensagem").innerHTML =randomIntFromInterval(1,10);
-      }else{
-        document.getElementById("mensagem").innerHTML =randomIntFromInterval(1,8);
-          }
+	let tipoDado=document.getElementById("valoresDados").value;
+	let resultado;
+	document.getElementById("seunumero").innerHTML = "Seu número é:";
+	if (tipoDado== "D4") {   
+		resultado = randomIntFromInterval(1,4);
+		document.getElementById("mensagem").innerHTML = resultado;
+	} else if (tipoDado=="D12") {
+		resultado = randomIntFromInterval(1,12);
+		document.getElementById("mensagem").innerHTML = resultado;
+	}else if (tipoDado=="D10") {
+		resultado = randomIntFromInterval(1,10);
+		document.getElementById("mensagem").innerHTML = resultado;
+	}else{
+		resultado = randomIntFromInterval(1,8);
+		document.getElementById("mensagem").innerHTML = resultado;
+	}
+	enviarMensagemDado(tipoDado,resultado)
 }
 
 document.getElementById("sortear").onclick = function() {dado()};
+
+function enviarMensagemDado(tipoDado, resultado){
+	let msg = usuarioLogado+" rolou o "+tipoDado+" e teve "+resultado+"!";
+	console.log(msg);
+	escrever(msg);
+}
